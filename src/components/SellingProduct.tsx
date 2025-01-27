f29accbcad12fb5416d02331396a79ebe86d0347
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2025-01-27 16:39:39 +0500

    SellingProduct.tsx

--- src/components/SellingProduct.tsx
+++ src/components/SellingProduct.tsx
@@ -10,6 +10,7 @@ import { Heart, ShoppingCart, Star, Expand } from "lucide-react";
 import { groq } from "next-sanity";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface Product {
   _id: string;
